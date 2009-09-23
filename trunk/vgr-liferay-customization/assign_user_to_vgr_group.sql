08cd11fb6d23fbbcbb40d73cd373e5a73882cf51
jonas.liljenfeldt <jonas.liljenfeldt@1281beca-396f-11de-8e0c-53a27eea117e>
2009-09-23 11:34:24 +0000

    Added pgScript for assigning users to vgregion group in Liferay. /David and Jonas

---
+++ trunk/vgr-liferay-customization/assign_user_to_vgr_group.sql
@@ -0,0 +1,20 @@
+﻿CREATE OR REPLACE FUNCTION assign_users_to_vgr_group() RETURNS refcursor AS $$
+DECLARE
+    ref refcursor;
+    uid integer;
+
+--vgr group id: 7764636
+BEGIN
+	OPEN ref FOR SELECT userid FROM user_;
+	LOOP
+		FETCH ref INTO uid;
+			IF  NOT FOUND THEN
+			EXIT;  -- exit loop
+			END IF;
+			insert into users_groups values (uid, 7764636);
+	END LOOP;
+	RETURN uid;
+END;
+$$ LANGUAGE plpgsql;
+
+SELECT assign_users_to_vgr_group();
