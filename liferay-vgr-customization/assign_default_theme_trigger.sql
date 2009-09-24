b09145e073e549bcdcb0651a91995d7b838de538
jonas.liljenfeldt <jonas.liljenfeldt@1281beca-396f-11de-8e0c-53a27eea117e>
2009-09-24 15:06:41 +0000

    Added trigger which sets default theme

---
+++ liferay-vgr-customization/assign_default_theme_trigger.sql
@@ -0,0 +1,16 @@
+﻿CREATE OR REPLACE FUNCTION update_default_user_theme() RETURNS trigger AS $$
+BEGIN
+     --RAISE NOTICE 'NEW.layoutsetid is currently %', NEW.layoutsetid;
+     UPDATE layoutset set themeid = 'vgrtheme_WAR_vgrtheme' WHERE layoutsetid = NEW.layoutsetid;
+     --RAISE NOTICE 'NEW.themeid is currently %', NEW.themeid;
+     --NEW.themeid := 'vgrtheme_WAR_vgrtheme';
+     RETURN NEW;
+END
+$$ LANGUAGE plpgsql;
+
+--Drop existing trigger
+DROP TRIGGER update_default_user_theme ON layoutset;
+
+--Add trigger
+CREATE TRIGGER update_default_user_theme AFTER INSERT ON layoutset
+  FOR EACH ROW EXECUTE PROCEDURE update_default_user_theme();
