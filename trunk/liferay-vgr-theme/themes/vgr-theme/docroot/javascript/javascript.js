0a796e5ba1894a0b41d0ab87e3aea0a35f83f2a2
aasplund@gmail.com <aasplund@gmail.com@1281beca-396f-11de-8e0c-53a27eea117e>
2010-09-01 06:18:07 +0000

    Adding slide theme for start page (100/50/50), missed some files in previous commit

--- trunk/liferay-vgr-theme/themes/vgr-theme/docroot/javascript/javascript.js
+++ trunk/liferay-vgr-theme/themes/vgr-theme/docroot/javascript/javascript.js
@@ -21,11 +21,11 @@ function() {
 function toggleNotifications(hide) {
   var $main = jQuery('#main-container');
   var $slide = jQuery('#slide-container');
-  $main.animate({
-    width: hide ? "100%" : "80%" 
-  });
   $slide.animate({
     right: hide ? -$slide.outerWidth() : 0
+  });
+  $main.animate({
+    width: hide ? "100%" : "80%"
   });
   jQuery('#slide-buttons #hide').css("display", hide ? "none" : "inline");
   jQuery('#slide-buttons #show').css("display", hide ? "inline" : "none");
